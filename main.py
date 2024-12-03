052a3e4e48469d6bfccfd82205dcf0ecd0dc9b80
8unnu <samsdnkind0707@gmail.com>
2024-12-03 21:42:22 +0500

    vesion: 1.3 - Adding a room system, socketio code minifix and adding a chat notification system

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 import socketio
 
@@ -20,18 +19,65 @@ app.mount('/static', StaticFiles(directory="static"))
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 socket_app = socketio.ASGIApp(sio, app)
 
+rooms_numbers = []
+free_rooms_numbers = []
+rooms = {}
+
 @sio.event
 async def connect(sid, env):
-    logger.info(f"Client: {sid} has connect")
+    if len(free_rooms_numbers) != 0:
+        room_number = free_rooms_numbers[random.randint(0, len(free_rooms_numbers) - 1)]
+        free_rooms_numbers.remove(room_number)
+        room = f"room_{room_number}"
+
+        await sio.enter_room(sid, room=room)
+        rooms[room].append(sid)
+
+        await sio.emit("message", ["system", f"Your opponent has joined"], room=room, skip_sid=sid)
+        await sio.emit("message", ["system", f"You joined your opponent"], room=room, skip_sid=rooms[room][0])
+
+        logger.info(f"Client: {sid} has connect {room} to {rooms[room][0]}")
+    else:
+        room_number = 1
+        while room_number in rooms_numbers:
+            room_number = random.randint(1, 999)
+        room = f"room_{room_number}"
+
+        await sio.enter_room(sid, room=room)
+        rooms_numbers.append(room_number)
+        free_rooms_numbers.append(room_number)
+        rooms[room] = [sid]
+
+        await sio.emit("message", ["system", f"You've join {room}. Expect an opponent."], room=room)
+
+        logger.info(f"Client: {sid} has connect {room} and wait your opponent")
+
 
 @sio.on("message")
 async def message(sid, data):
-    await sio.emit('message', [data['username'], data['message']], skip_sid=sid)
+    user_room = [i for i in sio.rooms(sid) if i != sid][0]
+    await sio.emit('message', [data['username'], data['message']], room=user_room, skip_sid=sid)
     logger.info(f"{sid}({data['username']}) say {data['message']}")
 
 @sio.event
 async def disconnect(sid):
-    logger.info(f"Client: {sid} has disconnect")
+    user_rooms = sio.rooms(sid)
+    user_room = [i for i in user_rooms if i != sid][0]
+
+    await sio.emit("message", ["system", "Your opponent is out, the room is closed"], room=user_room)
+
+    for id in rooms[user_room]:
+        await sio.disconnect(id)
+    await sio.close_room(user_room)
+
+    room_number = int(user_room.split("_")[1])
+    if room_number in rooms_numbers:
+        rooms_numbers.remove(room_number)
+    try:
+        rooms.pop(user_room)
+    except KeyError:
+        pass
+    logger.info(f"room_{room_number} delete and users has disconnect")
 
 @app.get("/")
 async def index(request: Request):
---
+++ test.py
@@ -0,0 +1,12 @@
+# arr = {"room_1": []}
+#
+# arr["room_1"].append("bb")
+# print(arr["room_1"])
+# arr["room_1"].append("gg")
+# print(arr["room_1"])
+# print(arr['room_1'][0])
+# arr["room_1"] = "lox"
+# print(arr["room_1"])
+
+arr = [1]
+print("1" in arr)
